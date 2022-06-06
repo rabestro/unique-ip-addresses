f468cd20f198475405c1f9a34a002c45808d629f
Jegors Cemisovs <jegors.cemisovs@accenture.com>
2022-06-06 21:19:18 +0300

    Add test for IPv4Set class

---
+++ src/test/groovy/lv/id/jc/ipv4/IPv4SetTest.groovy
@@ -0,0 +1,45 @@
+package lv.id.jc.ipv4
+
+import spock.lang.Issue
+import spock.lang.Narrative
+import spock.lang.PendingFeature
+import spock.lang.Specification
+import spock.lang.Subject
+import spock.lang.Title
+
+@Issue('1')
+@Title('IPv4Set')
+@Narrative('Container for storing a set of IP addresses')
+class IPv4SetTest extends Specification {
+
+    @Subject
+    def container = new IPv4Set()
+
+    def 'should return zero for an empty container'() {
+        expect:
+        container.size() == 0
+    }
+
+    def 'should not thrown an exception for correct ip address'() {
+        when:
+        container.add(ipAddress)
+
+        then:
+        noExceptionThrown()
+
+        where:
+        ipAddress << ['0.0.0.0', '255.255.255.255', '128.21.0.15']
+    }
+
+    @PendingFeature
+    def 'should thrown an exception if wrong ip address'() {
+        when:
+        container.add(ipAddress)
+
+        then:
+        thrown(Exception)
+
+        where:
+        ipAddress << ['0.0.0.', '256.255.255.255', '-128.21.0.15']
+    }
+}
--- src/test/groovy/lv/id/jc/ipv4/MainTest.groovy
+++ src/test/groovy/lv/id/jc/ipv4/MainTest.groovy
@@ -1,8 +1,14 @@
 package lv.id.jc.ipv4
 
+import spock.lang.Issue
+import spock.lang.Narrative
 import spock.lang.PendingFeature
 import spock.lang.Specification
+import spock.lang.Title
 
+@Issue('1')
+@Title('Application')
+@Narrative('Integration test for the console application')
 class MainTest extends Specification {
 
     def 'should throw an exception if no file name specified'() {
