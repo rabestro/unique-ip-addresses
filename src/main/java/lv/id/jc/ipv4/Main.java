15d90a5a5053331613dafed0bfe49e3c833c8938
Jegors Cemisovs <jegors.cemisovs@accenture.com>
2022-06-06 20:39:34 +0300

    Add an empty Main class with only JavaDoc

---
+++ src/main/java/lv/id/jc/ipv4/Main.java
@@ -0,0 +1,20 @@
+package lv.id.jc.ipv4;
+
+/**
+ * Console application for counting unique addresses in a text file
+ *
+ */
+public class Main {
+    /**
+     * Application start point
+     *
+     * The program takes a filename as a command line argument.
+     * The file must contain one address per line. The program
+     * prints the number of unique addresses in this file.
+     *
+     * @param args - path to the test file in the first argument
+     * @throws IndexOutOfBoundsException if no arguments provided
+     */
+    public static void main(String[] args) {
+    }
+}
