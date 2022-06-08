8815de1f0f2a3be5c1b40ead15e62866e220aa1d
Jegors Cemisovs <jegors.cemisovs@accenture.com>
2022-06-08 21:27:47 +0300

    Update javadoc

--- src/main/java/lv/id/jc/ipcounter/IpCounter.java
+++ src/main/java/lv/id/jc/ipcounter/IpCounter.java
@@ -6,7 +6,7 @@ import java.util.stream.Stream;
 /**
  * Counter of unique IPv4 addresses.
  *
- * Counts the number of unique IPv4 addresses in a string stream.
+ * Processes a stream of strings representing IPv4 addresses and counting unique ones.
  */
 public interface IpCounter extends ToLongFunction<Stream<String>> {
 }
