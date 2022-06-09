aa8bc51b4fe2b3cd4ca9d8999ea8bc65ccd172c4
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-06-09 23:12:33 +0300

    Add two.txt for test

--- src/test/groovy/lv/id/jc/ipcounter/MainTest.groovy
+++ src/test/groovy/lv/id/jc/ipcounter/MainTest.groovy
@@ -54,4 +54,21 @@ class MainTest extends Specification {
         expected << [1, 3, 4, 0]
     }
 
+    def 'test'() {
+        when: 'we run the program with file name as parameter'
+        Main.main fileName
+
+        and: 'the program prints unique number of ip addresses'
+        def result = new Scanner(out.toString()).nextInt()
+
+        then: 'the result as expected'
+        result == expected
+
+        where: 'name of text file in test resources'
+        name  | expected
+        'two' | 2
+
+        and:
+        fileName = getClass().getResource("/${name}.txt").path
+    }
 }
